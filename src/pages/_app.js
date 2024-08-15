3f2ef61fe333cd55d991de0f54110d8dc6f8960c
Muhammad Hanan <hanan.ppqita@gmail.com>
2024-08-15 11:55:05 +0700

    Memperbaiki masalah deploy di vercel 3

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,7 @@
 import "@/styles/globals.css";
-import { DataProvider } from "./context";
 
 export default function App({ Component, pageProps }) {
   return (
-    <DataProvider>
       <Component {...pageProps} />
-    </DataProvider>
   );
 }
